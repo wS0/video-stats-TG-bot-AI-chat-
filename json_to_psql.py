85da1921529f5a42adc597c42236e3bb604624ea
Yaroslav Ponomarev <g802.11@gmail.com>
2025-12-17 12:08:40 +0300

    Delete timezone
    Убрал imezone, чтобы не было искажения данных DateTime из-за поправки на окружение, в котором происходит загрузка данных из JSON в БД.

--- json_to_psql.py
+++ json_to_psql.py
@@ -14,13 +14,13 @@ class Video(Base):
     __tablename__ = 'videos'
     id = Column(String, primary_key=True)
     creator_id = Column(String)
-    video_created_at = Column(DateTime)
+    video_created_at = Column(DateTime(timezone=False))
     views_count = Column(Integer)
     likes_count = Column(Integer)
     reports_count = Column(Integer)
     comments_count = Column(Integer)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    created_at = Column(DateTime(timezone=False))
+    updated_at = Column(DateTime(timezone=False))
 
 
 class VideoSnapshot(Base):
@@ -35,8 +35,8 @@ class VideoSnapshot(Base):
     delta_likes_count = Column(Integer)
     delta_reports_count = Column(Integer)
     delta_comments_count = Column(Integer)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    created_at = Column(DateTime(timezone=False))
+    updated_at = Column(DateTime(timezone=False))
 
 
 # Создаем таблицы, если не существуют
@@ -51,16 +51,16 @@ with open('videos.json', 'r') as f:
 
 for v in data['videos']:
     # Преобразуем строки в datetime
-    v['video_created_at'] = datetime.fromisoformat(v['video_created_at'].replace('Z', '+00:00'))
-    v['created_at'] = datetime.fromisoformat(v['created_at'].replace('Z', '+00:00'))
-    v['updated_at'] = datetime.fromisoformat(v['updated_at'].replace('Z', '+00:00'))
+    v['video_created_at'] = datetime.fromisoformat(v['video_created_at'].replace('Z', '+00:00')).replace(tzinfo=None)
+    v['created_at'] = datetime.fromisoformat(v['created_at'].replace('Z', '+00:00')).replace(tzinfo=None)
+    v['updated_at'] = datetime.fromisoformat(v['updated_at'].replace('Z', '+00:00')).replace(tzinfo=None)
 
     video = Video(**{k: v[k] for k in v if k != 'snapshots'})
     session.add(video)
 
     for s in v['snapshots']:
-        s['created_at'] = datetime.fromisoformat(s['created_at'].replace('Z', '+00:00'))
-        s['updated_at'] = datetime.fromisoformat(s['updated_at'].replace('Z', '+00:00'))
+        s['created_at'] = datetime.fromisoformat(s['created_at'].replace('Z', '+00:00')).replace(tzinfo=None)
+        s['updated_at'] = datetime.fromisoformat(s['updated_at'].replace('Z', '+00:00')).replace(tzinfo=None)
         snapshot = VideoSnapshot(**s)
         session.add(snapshot)
 
